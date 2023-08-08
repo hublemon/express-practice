381a0480be7a3b20c0a916c48faecdd804ae6a13
hublemon <113337611+hublemon@users.noreply.github.com>
2023-08-08 19:09:55 +0900

    server push

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import dayjs from 'dayjs';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const app=express();
+
+app.use(cors());
+app.use(helmet());
+
+const today=new Date();
+const todayToDayjs=dayjs(today).format("YYYY-MM-DD");
+console.log({todayToDayjs});
+
+const password="0000";
+const hashedPassword=bcrypt.hashSync(password,10);
+console.log(hashedPassword);
+
+const token=jwt.sign("1234","dfjkashoo12jpsad");
+console.log(token);
 
 app.get("/",(req,res)=>{
     res.send("Node.js강의 좋아요");
